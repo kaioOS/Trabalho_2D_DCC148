a1d476d4bf160bd9c112a45422d0cfd286242081
kaioOS <kos20111@hotmail.com>
2023-10-27 00:30:39 -0300

    Coletáveis e Partículas
    .Sistema de partícula ao cortar árvores
    .Drops das árvores
    .Coletar os drops

---
+++ Trabalho_1_2D_DCC148/Assets/Scripts/Craft/Tree.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    [SerializeField] private float tree_Health;
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject woodPrefab;
+    [SerializeField] private int totalWood;
+    [SerializeField] private ParticleSystem leafs;
+     public void onHit()
+    {
+        tree_Health--;
+ 
+        anim.SetTrigger("hit");
+        leafs.Play();
+        
+        if(tree_Health == 0) //Árvore destruída
+        {
+            totalWood = Random.Range(1,5);
+            for (int i = 0; i < totalWood; i++)
+            {
+                //Gera o coletável de madeira
+                Instantiate(
+                woodPrefab, 
+                transform.position + new Vector3(Random.Range(-0.5f,0.5f),
+                Random.Range(-0.5f,0.5f),0f), 
+                transform.rotation); 
+            }
+            
+
+            anim.SetTrigger("cut");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Axe"))
+        {
+            onHit();
+        }
+    }
+}
